7ed942933e47aa8779b05750f299d6b58f83ee74
HugoZumpano <hugo_zumpano@hotmail.com.br>
2024-12-12 21:22:26 -0300

    fix: tirando dependencia so screnshot de colorir o elemento

--- ntt.poc.treinamento/src/test/java/resources/utils/ScreenshotUtil.java
+++ ntt.poc.treinamento/src/test/java/resources/utils/ScreenshotUtil.java
@@ -14,13 +14,23 @@ public class ScreenshotUtil {
 
     public static void screenshotElement(By locator) throws IOException {
         String time = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss-SS").format(new Date());
-        WebElement element = driver.findElement(locator);
-        String originalStyle = element.getDomProperty("style");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].style.border='3px solid red'", element);
+        colorElement(locator);
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String currentDir = System.getProperty("user.dir");
         FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + time + ".png"));
+        discolorElement(locator);
+    }
+
+    public static void colorElement(By locator){
+        WebElement element = driver.findElement(locator);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].style.border='3px solid red'", element);
+        }
+
+    public static void discolorElement(By locator){
+        WebElement element = driver.findElement(locator);
+        String originalStyle = element.getDomProperty("style");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].setAttribute('style', arguments[1])", element, originalStyle);
     }
 }
